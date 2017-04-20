e275fd20a16368aee62dad721c69120cefb72cef
William Pickering <willpicky@gmail.com>
2017-04-20 10:04:03 +0100

    Product model barcode set as unique

--- models/products.js
+++ models/products.js
@@ -2,9 +2,19 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const productSchema = new Schema({
-    code: String,
-    name: String,
-    packaging: String
+    code: {
+      type: String,
+      index: {
+        unique: true,
+        dropDups: true
+      }
+    },
+    name: {
+      type: String
+    },
+    packaging: {
+      type: String
+    }
 });
 
 module.exports = mongoose.model('products', productSchema);
